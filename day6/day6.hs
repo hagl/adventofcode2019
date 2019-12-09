da341b29bc44377b3c5da4b3949bdf9eac0c184c
hagl <haraldg@kahoot.com>
2019-12-09 15:26:46 +0100

    day 9

--- day6/day6.hs
+++ day6/day6.hs
@@ -1,7 +1,9 @@
-import           Data.List
+import           Data.List (head, tail, groupBy, sort, find, reverse)
 import           Data.List.Split       (splitOn)
-import           Data.Vector.Primitive (Vector, fromList, toList, (!), (//))
 import           System.Environment    (getArgs)
+import Data.Map.Strict (Map, empty, insert, (!), findWithDefault)
+import Control.Monad (join)
+import Data.Maybe ( fromJust)
 
 data Node =
   Node String
@@ -14,19 +16,43 @@ data Pair =
        String
   deriving (Eq, Ord, Show)
 
-mkPair :: String -> Pair
-mkPair str =
-  let list = splitOn ")" str
-  in Pair (head list) (head (tail list))
+commonPrefix :: (Eq a) => Int -> [a] -> [a] -> Int
+commonPrefix n [] _ = n
+commonPrefix n _ [] = n
+commonPrefix n (h1:t1) (h2:t2) = if (h1 /= h2) then n else commonPrefix (n + 1) t1 t2
+
+mkPair :: String -> [String]
+mkPair str = splitOn ")" str
+createNode :: (Map String [String]) -> Int -> String -> Node
+createNode lookupTable d str = 
+  let children = findWithDefault [] str lookupTable in
+    Node str d (map (createNode lookupTable (d +1)) children)
+
+count :: Node -> Int
+count (Node _ d children) = d + (sum $ map count children)
 
 -- parseTree :: [String] -> Node
 parseTree strs =
   let pairs = map mkPair strs
-  in (groupBy (\(Pair a _) -> \(Pair b _) -> a == b) $ sort pairs)
+      grouped = groupBy (\(a : _) -> \(b : _) -> a == b) $ sort pairs
+      lookupTable = foldr (\l -> insert (head $ head l) (map (\(_:x:_) -> x) l)) empty grouped
+      node = createNode lookupTable 0 "COM"
+  in node
+
+path :: [String] -> String -> Node -> Maybe [String]
+path base str (Node n _ children) = if (str == n) then Just base else join $ find (\p -> p /= Nothing) $ map (path (n : base) str) children 
+
 
 processFile inputFile = do
   input <- readFile inputFile
-  putStrLn (unlines $ map show $ parseTree (lines input))
+  let root = parseTree (lines input) 
+  let pathYOU = reverse $ fromJust $ path [] "YOU" root
+  let pathSAN = reverse $ fromJust $ path [] "SAN" root
+  putStrLn (show $ (length pathYOU) + (length pathSAN) - (2 * commonPrefix 0 pathYOU pathSAN))
+  putStrLn (show $ reverse $ fromJust $ path [] "YOU" root)
+  putStrLn (show $ reverse $ fromJust $ path [] "SAN" root)
+    -- putStrLn (show $ count $ root)
+    -- putStrLn (unlines $ map show $ parseTree (lines input))
 
 test = do
   processFile "input.txt"
